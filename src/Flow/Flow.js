b74484672500e88dfc5895621923997dcb066a91
Hedvig <hedvig.reutersward@gmail.com>
2018-05-04 15:36:21 +0200

    flow uppdaterad

--- src/Flow/Flow.js
+++ src/Flow/Flow.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import { Slide } from 'react-slideshow-image';
 
-const images = [
+let images = [
 'https://img.youtube.com/vi/g3zsMnNpgsA/maxresdefault.jpg',
 'https://img.youtube.com/vi/2lx-RNW-JlE/maxresdefault.jpg',
 'https://img.youtube.com/vi/2gfOO9c53Zs/maxresdefault.jpg',
 'https://img.youtube.com/vi/ISSxMHsvvyw/maxresdefault.jpg'
 ]
+
+
 
 class Flow extends Component {
 
@@ -19,10 +21,12 @@ class Flow extends Component {
   }
 
   componentDidMount() {
+
     this.props.model.getVideos().then(video => {
       this.setState({
         status: 'LOADED',
-        resultyt: video
+        resultyt: video,
+
       })
     }).catch(() => {
       this.setState({
@@ -31,31 +35,21 @@ class Flow extends Component {
     })
   }
 
+  onMouseEnterHandler() {
+    console.log("hovvraaar");
+  }
+
   render(){
-    // console.log(finalURL);
-    //console.log(this.state.resultyt);
+    
+
+
 
     return(
       <div>
+      <div className="col-md-1"></div>
       <div className="carouselArea col-md-10">
-          {/*{
-            this.state.resultyt.map((link, i) => {
-            console.log(link);
-            var frame =
-            <div key={i} className="col-md-2 youtube">
-            <img   id="carouselThumbnail" width="840" height="472.5" src={"hej"} alt="video" frameBorder="0" allowFullScreen/>
-            </div>
-            return frame;
-            })
-          }*/}
-
-
-          <Slide
-          duration = {5000}
-          transitionDuration={1000}
-          images= {images}
-          />
-          </div>
+          <iframe width="840" height="472.5" src='https://www.youtube.com/embed/bm_zLDuF7LM' frameBorder="0" allowFullScreen></iframe>  
+      </div>
 
           {
             this.state.resultyt.map((link, i) => {
@@ -63,7 +57,7 @@ class Flow extends Component {
               <div>
               <div className="youtubeArea">
               <div key={i} className="col-md-2 youtube">
-              <iframe width="280" height="157.5" src={link} frameBorder="0" allowFullScreen>
+              <iframe width="280" height="157.5" src={link} frameBorder="0"  onMouseOver={this.onMouseEnterHandler} onMouseEnter={this.onMouseEnterHandler} allowFullScreen >
               </iframe>
               </div>
               </div>
